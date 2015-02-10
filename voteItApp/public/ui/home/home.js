2b39c6074ac3b842fae912a49c4dd60cf78f178e
shahanak <shahana.k@experionglobal.com>
2015-02-10 18:12:57 +0530

    Vote It App added

--- voteItApp/public/ui/home/home.js
+++ voteItApp/public/ui/home/home.js
@@ -1,7 +1,9 @@
 angular.module('voteIt').controller('HomeCtrl',function($scope,$firebase){
 
 	var ref = new window.Firebase("https://blinding-heat-4724.firebaseio.com/news");  
-	var fb = $firebase(ref);	
+	
+	var fb = $firebase(ref);
+
 	$scope.show = function(){
 		$scope.showDiv = true;
 	};
@@ -24,18 +26,63 @@ angular.module('voteIt').controller('HomeCtrl',function($scope,$firebase){
 		$scope.showDiv = false;
 	};
 	var syncObject = fb.$asObject();
-	syncObject.$bindTo($scope, 'newses');	
+	syncObject.$bindTo($scope, 'newses');		
 		
-		$scope.up = function(details){
-			
-			details.voteCount ++;	
-		}
-		$scope.down = function(details){
-			
+		$scope.up = function(details,newses){				
+				details.voteCount ++;	
+				var arrObj = [];
+				var i = 0 ;
+				for(var key in newses){
+					arrObj[i]  = newses[key];
+					i++;				
+				}
+			console.log(arrObj);
+			for(var i = 2; i<arrObj.length; i++){
+				for(var j = 2; j<arrObj.length - 1; j++){
+					if(arrObj[j].voteCount < arrObj[j+1].voteCount){
+						var t = arrObj[j];
+						arrObj[j] = arrObj[j+1];
+						arrObj[j+1] = t;
+					}
+				}
+			}
+			i = 0 ;
+			console.log(arrObj);
+			for(var key in newses){
+				newses[key]  = arrObj[i];
+				i++;				
+			}
+			console.log(newses);
+		};
+		$scope.down = function(details,newses){
+
 			details.voteCount --;
+			var arrObj = [];
+				var i = 0 ;
+				for(var key in newses){					
+				arrObj[i]  = newses[key];
+				i++;						
+			}
+			console.log(arrObj);
+			for(var i = 2; i<arrObj.length; i++){
+				for(var j = 2; j<arrObj.length - 1; j++){
+					if(arrObj[j].voteCount < arrObj[j+1].voteCount){
+						var t = arrObj[j];
+						arrObj[j] = arrObj[j+1];
+						arrObj[j+1] = t;
+					}
+				}
+			}
+			i = 0 ;
+			console.log(arrObj);
+			for(var key in newses){
+				newses[key]  = arrObj[i];
+				i++;				
+			}
+			console.log(newses);
 
 			
-		}
+		};
 
 
 });
